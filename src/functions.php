242eb1d6501600f62051ac72c2a0f3d0d08b4027
Stefan Repac <repac1987@gmail.com>
2018-05-15 15:04:22 +0200

    Show Admin bar just for administrator

--- src/functions.php
+++ src/functions.php
@@ -282,9 +282,9 @@ function html5_blank_view_article( $more ) {
     return '... <a class="view-article" href="' . get_permalink( $post->ID ) . '">' . esc_html_e( 'View Article', 'html5blank' ) . '</a>';
 }
 
-// Remove Admin bar
-function remove_admin_bar() {
-    return false;
+// Show Admin bar just for administrator
+if ( ! current_user_can( 'manage_options' ) ) {
+    show_admin_bar( false );
 }
 
 // Remove 'text/css' from our enqueued stylesheet
@@ -393,7 +393,6 @@ add_filter( 'the_category', 'remove_category_rel_from_category_list' ); // Remov
 add_filter( 'the_excerpt', 'shortcode_unautop' ); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
 add_filter( 'the_excerpt', 'do_shortcode' ); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
 add_filter( 'excerpt_more', 'html5_blank_view_article' ); // Add 'View Article' button instead of [...] for Excerpts
-add_filter( 'show_admin_bar', 'remove_admin_bar' ); // Remove Admin bar
 add_filter( 'style_loader_tag', 'html5_style_remove' ); // Remove 'text/css' from enqueued stylesheet
 add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 ); // Remove width and height dynamic attributes to thumbnails
 add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 ); // Remove width and height dynamic attributes to post images
